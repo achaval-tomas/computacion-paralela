903c287ac9100f7b6abcf2722919a366e819ef2e
achaval-tomas <tomasachaval@mi.unc.edu.ar>
2025-03-17 22:28:57 +0000

    Print cells per us una vez por ejecución

--- headless.c
+++ headless.c
@@ -7,9 +7,9 @@
 
    Description:
 
-	This code is a simple prototype that demonstrates how to use the
-	code provided in my GDC2003 paper entitles "Real-Time Fluid Dynamics
-	for Games". This code uses OpenGL and GLUT for graphics and interface
+        This code is a simple prototype that demonstrates how to use the
+        code provided in my GDC2003 paper entitles "Real-Time Fluid Dynamics
+        for Games". This code uses OpenGL and GLUT for graphics and interface
 
   =======================================================================
 */
@@ -129,37 +129,11 @@ static void react(float* d, float* u, float* v)
 
 static void one_step(void)
 {
-    static int times = 1;
-    static double start_t = 0.0;
-    static double one_second = 0.0;
-    static double react_ns_p_cell = 0.0;
-    static double vel_ns_p_cell = 0.0;
-    static double dens_ns_p_cell = 0.0;
-
-    start_t = wtime();
     react(dens_prev, u_prev, v_prev);
-    react_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
 
-    start_t = wtime();
     vel_step(N, u, v, u_prev, v_prev, visc, dt);
-    vel_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
 
-    start_t = wtime();
     dens_step(N, dens, dens_prev, u, v, diff, dt);
-    dens_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
-
-    if (1.0 < wtime() - one_second) { /* at least 1s between stats */
-        printf("%lf, %lf, %lf, %lf: ns per cell total, react, vel_step, dens_step\n",
-               (react_ns_p_cell + vel_ns_p_cell + dens_ns_p_cell) / times,
-               react_ns_p_cell / times, vel_ns_p_cell / times, dens_ns_p_cell / times);
-        one_second = wtime();
-        react_ns_p_cell = 0.0;
-        vel_ns_p_cell = 0.0;
-        dens_ns_p_cell = 0.0;
-        times = 1;
-    } else {
-        times++;
-    }
 }
 
 
@@ -207,9 +181,14 @@ int main(int argc, char** argv)
         exit(1);
     }
     clear_data();
+
+    double start_t = wtime();
     for (i = 0; i < 2048; i++) {
         one_step();
     }
+    double total_us = 1.0e6 * (wtime() - start_t);
+    printf("cells_per_us: %lf", (double)(2048 * (N + 2) * (N + 2)) / total_us);
+
     free_data();
 
     exit(0);
